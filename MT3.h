bf857e24e322aabe7db40429d8b07a08f53ffeaf
FUMIFUMI0829 <k025g0049@g.neec.ac.jp>
2026-04-24 10:02:53 +0900

    a

--- MT3.h
+++ MT3.h
@@ -2,23 +2,23 @@
 #include "Vector3.h"
 
 
-	// 加算
-	Vector3 Add(const Vector3& v1, const Vector3& v2);
+// 加算
+Vector3 Add(const Vector3& v1, const Vector3& v2);
 
-	// 減算
-	Vector3 Subtract(const Vector3& v1, const Vector3& v2);
+// 減算
+Vector3 Subtract(const Vector3& v1, const Vector3& v2);
 
-	// スカラー倍
-	Vector3 Multiply(float scalar, const Vector3& v);
+// スカラー倍
+Vector3 Multiply(float scalar, const Vector3& v);
 
-	// 内積
-	float Dot(const Vector3& v1, const Vector3& v2);
+// 内積
+float Dot(const Vector3& v1, const Vector3& v2);
 
-	// 長さ
-	float Length(const Vector3& v);
+// 長さ
+float Length(const Vector3& v);
 
-	// 正規化
-	Vector3 Normalize(const Vector3& v);
+// 正規化
+Vector3 Normalize(const Vector3& v);
 
-	// 画面表示
-	void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
+// 画面表示
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
