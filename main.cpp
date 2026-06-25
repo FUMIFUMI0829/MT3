b1e90ee4a5f061f89488c433cdab50642fa9cc39
TAKATAN0829 <ohitorisamadesune@gmail.com>
2026-06-25 21:32:24 +0900

    kann

--- main.cpp
+++ main.cpp
@@ -13,66 +13,27 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
-	/*Vector3 rotate{ 0.f,0.f,0.f };
-	OBB obb{
-		.center{-1.f,0.f,0.f},
-		.orientations{{1.f,0.f,0.f},
-					  {0.f,1.f,0.f},
-					  {0.f,0.f,1.f}},
-		.size{0.5f,0.5f,0.5f}
-	};*/
-
-	/*Sphere sphere{
-		.center{0.f,0.f,0.f},
-		.radius{0.5f}
-	};*/
-
-	/*Segment segment{
-		.origin{-0.8f, -0.3f, 0.0f},
-		.diff{0.5f, 0.5f, 0.5f}
-	};
-
-	Vector3 rotate1{ 0.0f, 0.0f, 0.0f };
-	Vector3 rotate2{ -0.05f, -2.49f, 0.15f };
-	OBB obb1{
-		.center{0.0f, 0.0f, 0.0f},
-		.orientations = {{1.0f, 0.0f, 0.0f},
-						 {0.0f, 1.0f, 0.0f},
-						 {0.0f, 0.0f, 1.0f}},
-		.size{0.83f, 0.26f, 0.24f}
-	};
-
-	OBB obb2{
-		.center{0.9f, 0.66f, 0.78f},
-		.orientations = {{1.0f, 0.0f, 0.0f},
-						 {0.0f, 1.0f, 0.0f},
-						 {0.0f, 0.0f, 1.0f}},
-		.size{0.5f, 0.37f, 0.5f}
-	};*/
-
 	Vector3 cameraTranslate{ 0.0f, 1.9f, -6.49f };
 	Vector3 cameraRotate{ 0.26f, 0.0f, 0.0f };
 
-	//Matrix4x4 rotateMatrix = Multiply(MakeRotateXMatrix(rotate.x), Multiply(MakeRotateYMatrix(rotate.y), MakeRotateYMatrix(rotate.z)));
-
-	////回転行列から軸を抽出
-	//obb.orientations[0].x = rotateMatrix.m[0][0];
-	//obb.orientations[0].y = rotateMatrix.m[0][1];
-	//obb.orientations[0].z = rotateMatrix.m[0][2];
-
-	//obb.orientations[1].x = rotateMatrix.m[1][0];
-	//obb.orientations[1].y = rotateMatrix.m[1][1];
-	//obb.orientations[1].z = rotateMatrix.m[1][2];
-
-	//obb.orientations[2].x = rotateMatrix.m[2][0];
-	//obb.orientations[2].y = rotateMatrix.m[2][1];
-	//obb.orientations[2].z = rotateMatrix.m[2][2];
-
-	Vector3 controlPoints[3] = {
-		{ -0.8f, 0.58f, 1.0f },
-		{ 1.76f, 1.0f, -0.3f },
-		{ 0.94f, -0.7f, 2.3f },
+	Vector3 translates[3] = {
+		{0.2f, 1.0f, 0.0f},
+		{0.4f, 0.0f, 0.0f},
+		{0.3f, 0.0f, 0.0f},
 	};
+	Vector3 rotates[3] = {
+		{0.0f, 0.0f, -6.8f},
+		{0.0f, 0.0f, -1.4f},
+		{0.0f, 0.0f,  0.0f},
+	};
+	Vector3 scales[3] = {
+		{1.0f, 1.0f, 1.0f},
+		{1.0f, 1.0f, 1.0f},
+		{1.0f, 1.0f, 1.0f},
+	};
+
+
+	unsigned int jointColors[3] = { RED, GREEN, BLUE };
 
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
@@ -101,19 +62,38 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
 		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
 
-		ImGui::DragFloat3("controlPoints[0]", &controlPoints[0].x, 0.01f);
-		ImGui::DragFloat3("controlPoints[1]", &controlPoints[1].x, 0.01f);
-		ImGui::DragFloat3("controlPoints[2]", &controlPoints[2].x, 0.01f);
+		ImGui::Separator();
+		ImGui::DragFloat3("Shoulder Translate", &translates[0].x, 0.01f);
+		ImGui::DragFloat3("Shoulder Rotate", &rotates[0].x, 0.01f);
+		ImGui::DragFloat3("Shoulder Scale", &scales[0].x, 0.01f);
+
+		ImGui::Separator();
+		ImGui::DragFloat3("Elbow Translate", &translates[1].x, 0.01f);
+		ImGui::DragFloat3("Elbow Rotate", &rotates[1].x, 0.01f);
+		ImGui::DragFloat3("Elbow Scale", &scales[1].x, 0.01f);
+
+		ImGui::Separator();
+		ImGui::DragFloat3("Hand Translate", &translates[2].x, 0.01f);
+		ImGui::DragFloat3("Hand Rotate", &rotates[2].x, 0.01f);
+		ImGui::DragFloat3("Hand Scale", &scales[2].x, 0.01f);
 
 		ImGui::End();
 #endif
 
-		unsigned int objColor = WHITE;
+		Matrix4x4 worldMatrices[3];
+		worldMatrices[0] = MakeAffineMatrix(scales[0], rotates[0], translates[0]);
+		worldMatrices[1] = Multiply(MakeAffineMatrix(scales[1], rotates[1], translates[1]), worldMatrices[0]);
+		worldMatrices[2] = Multiply(MakeAffineMatrix(scales[2], rotates[2], translates[2]), worldMatrices[1]);
 
-		/*if (IsCollision(obb1, obb2)) {
-			objColor = RED;
-		}*/
+		Vector3 worldPos[3];
+		Sphere sphere[3];
+		for (int i = 0; i < 3; ++i) {
+			worldPos[i] = { worldMatrices[i].m[3][0], worldMatrices[i].m[3][1], worldMatrices[i].m[3][2] };
+			sphere[i] = { worldPos[i], 0.05f };
+		}
 
+		Segment seg01{ worldPos[0], Subtract(worldPos[1], worldPos[0]) };
+		Segment seg12{ worldPos[1], Subtract(worldPos[2], worldPos[1]) };
 		///
 		/// ↑更新処理ここまで
 		///
@@ -124,7 +104,11 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 		DrawGrid(viewProjectionMatrix, viewportMatrix);
 
-		DrawBezier(controlPoints[0], controlPoints[1], controlPoints[2], viewProjectionMatrix, viewportMatrix, objColor);
+		for (int i = 0; i < 3; ++i) {
+			DrawSphere(sphere[i], viewProjectionMatrix, viewportMatrix, jointColors[i]);
+		}
+		DrawSegment(seg01, viewProjectionMatrix, viewportMatrix, WHITE);
+		DrawSegment(seg12, viewProjectionMatrix, viewportMatrix, WHITE);
 
 		///
 		/// ↑描画処理ここまで
